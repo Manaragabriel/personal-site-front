6faffedab4e451934f58b6c9b578c39fbaed473f
Manaragabriel <gabrielmanara2010@hotmail.com>
2020-08-12 22:20:03 -0300

    Terminando por hora, fazer melhorias no final

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 
 
 function HomePage() {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible,  setIsVisible] = useState({about: false, portifolio:false});
   
   useEffect(function ()  {
       window.onscroll = onScroll
@@ -15,7 +15,7 @@ function HomePage() {
   const onScroll = (event) => {
     console.log(document.documentElement.scrollTop)
     if(document.documentElement.scrollTop > 400){
-      setIsVisible(true);
+      setIsVisible({portifolio:true});
     }
     console.log(isVisible);
   }
@@ -45,9 +45,24 @@ function HomePage() {
 
           <section id="portifolio" class="py-4">
             <div class="container">
-                <h4  className={ 'text-center  font-weight-bold ' + (isVisible ? 'visible slide-in-right ' : 'invisible')  }>Meu portifólio</h4>
+                <h3  className={ 'text-center  font-weight-bold ' + (isVisible.portifolio ? 'visible slide-in-right ' : 'invisible')  }>Meu portifólio</h3>
                 <div class="row">
-                  
+                  <div  className={ 'col-12 bg-white rounded p-2 ' + (isVisible.portifolio ? 'visible slide-in-right ' : 'invisible')  }>
+                     <div class="row">
+                        <div class="col-12 col-lg-3">
+                            <div class="image-container-valore text-center">
+                              <img class="h-100 slide-in-left" id="gabriel-photo" src="/valore.png" />
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-lg-9">
+                            <h4 class="font-weight-bold">Desenvolvedor Web - NSC Valore(Trabalhando atualmente)</h4>
+                            <p>Exercendo a função de desenvolvedor web, no qual trabalho com as tecnologias PHP,Codeigniter, Wordpress, Node e Vue js
+                              Trabalho prestando manutenção em sites, APP'S PWA e desenvolvendo Plugins para Wordpress
+                            </p>
+                        </div>
+                     </div>
+                  </div>
                 </div>
             </div>
           </section>
